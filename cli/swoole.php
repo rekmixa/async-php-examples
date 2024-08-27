bea30789bca01222aaa72db5f6e65f9a11977954
rekmixa <rekmixa@gmail.com>
2024-08-27 20:49:24 +0500

    update swoole example

--- cli/swoole.php
+++ cli/swoole.php
@@ -2,16 +2,22 @@
 
 declare(strict_types=1);
 
+use Clio\Console;
+
 require __DIR__ . '/../config/bootstrap.php';
 
-co::run(static function (): void {
-    go(static function (): void {
-        Co::sleep(1);
-        echo "Done 1\n";
+$time = microtime(true);
+
+co::run(static function () use ($time): void {
+    go(static function () use ($time): void {
+//        Co::sleep(1);
+        sleep(1); // даже с использованием блокирующей функции sleep, обе корутины завершили свое выполнение одновременно!
+        Console::output('Done 1: ' . (microtime(true) - $time));
     });
 
-    go(static function () {
-        Co::sleep(1);
-        echo "Done 2\n";
+    go(static function () use ($time): void {
+//        Co::sleep(1);
+        sleep(1);
+        Console::output('Done 2: ' . (microtime(true) - $time));
     });
 });
